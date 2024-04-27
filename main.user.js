c8579a92b41e58f0dd3000b0c4babb5ea7f6ea18
grj1234 <grj1234@protonmail.com>
2024-04-28 08:06:37 +0900

    Delete the old URL to saver.js

--- main.user.js
+++ main.user.js
@@ -14,9 +14,10 @@
 
 (function(){
 	if(!document.getElementById("HtmlSaverScript88A042C75A9B4ED99FFDFFD32ED33C38")) {
+		const p='<path to saver.js script>':
 		var a=document.createElement('script');
 		a.setAttribute('type','text/javascript');
-		a.setAttribute('src','//grj1234.web.fc2.com/userscript/src/nicopedia_editing_article_saver/saver.js?'+parseInt(Date.parse(new Date())/600000));
+		a.setAttribute('src',p+'?'+parseInt(Date.parse(new Date())/600000));
 		a.setAttribute('id','HtmlSaverScript88A042C75A9B4ED99FFDFFD32ED33C38');
 		document.body.appendChild(a);
 	}
